3854d3d2ddbc008daeaf5b2693c6c34fb462d66b
isabella <isabellaschmitz00@gmail.com>
2023-04-12 15:57:40 -0300

    ffsff

--- src/screens/CreatUser.jsx
+++ src/screens/CreatUser.jsx
@@ -2,11 +2,21 @@ import { View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 import { styles } from "../config/style";
 import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export default function CreateUser() {
   const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
   function cadastrarPessoa() {
     console.log(nome);
+    console.log(telefone);
+    // usar a função do firebase para cadastrar
+
+    const docRef = addDoc(collection(db, "pessoas"), {
+      nomeDaPessoa: nome,
+      telefone: telefone,
+    });
   }
 
   return (
@@ -19,6 +29,13 @@ export default function CreateUser() {
           mode="outlined"
           value={nome}
           onChangeText={setNome}
+        />
+        <TextInput
+          label={"Telefone"}
+          placeholder="digita seu telefone"
+          mode="outlined"
+          value={telefone}
+          onChangeText={setTelefone}
         />
         <Button onPress={cadastrarPessoa}>Cadastrar</Button>
       </View>
